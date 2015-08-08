4a5c4a8d1c302539aa5cc9cd135953a1ccb7013f
Loïck Piera <pyrech@gmail.com>
2015-08-08 16:48:05 +0200

    Fix CS

--- tests/Functional/Php4ConstructorTest.php
+++ tests/Functional/Php4ConstructorTest.php
@@ -8,6 +8,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 namespace Joli\Php7Checker\tests\Functional;
 
 class Php4ConstructorTest extends AbstractFunctionalTestCase
